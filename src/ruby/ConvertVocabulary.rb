a8b73ba8dba902d176c95aea354b42c7de38f9b7
Steven Jenkins <j.s.jenkins@jpl.nasa.gov>
2021-06-01 17:14:41 -0700

    relation range restriction pattern

--- src/ruby/ConvertVocabulary.rb
+++ src/ruby/ConvertVocabulary.rb
@@ -26,12 +26,13 @@ end
 
 class Concept
 
-  attr_reader :label, :name, :types
+  attr_reader :label, :name, :types, :rest_all
 
   def initialize(label, types = Set.new)
     @label = label
     @name = label_to_name(label)
     @types = types.respond_to?(:each) ? types : Set.new([types])
+    @rest_all = {}
   end
 
   def label_to_name(label)
@@ -42,6 +43,9 @@ class Concept
     lines = []
     lines << %(    @rdfs:label "#{@label}")
     lines << "    concept #{@name} :> #{@types.to_a.join(', ')} ["
+    @rest_all.each do |prop, range|
+      lines << "        restricts all relation #{prop} to #{range}"
+    end
     lines << "    ]\n"
     lines.join("\n")
   end
@@ -66,12 +70,19 @@ table.each do |row|
 
   if (role_cell = row['Configured Role Name'])
     role_label = role_cell.to_s.split('::').last.gsub(/ \[.*\z/, '')
-    cs.fetch(role_label) { |k| cs[k] = Concept.new(role_label, 'interface:System')}
+    sy_c = cs.fetch(role_label) { |k| cs[k] = Concept.new(role_label, 'interface:System')}
+    provides_if_rest_label = "#{role_label} Provided Interface"
+    provides_if_rest_c = cs.fetch(provides_if_rest_label) { |k| cs[k] = Concept.new(provides_if_rest_label, 'interface:Interface')}
+    sy_c.rest_all['interface:providesInterface'] = provides_if_rest_c.name
+    int_thr_port_rest_label = "#{role_label} Interacts Through Port"
+    int_thr_port_rest_c = cs.fetch(int_thr_port_rest_label) { |k| cs[k] = Concept.new(int_thr_port_rest_label, 'interface:Port')}
+    sy_c.rest_all['interface:interactsThrough'] = int_thr_port_rest_c.name
   end
 
   if (if_cell = row['Configured Interface Name'])
     if_label = if_cell.to_s.split('::').last.gsub(/ \[.*\z/, '')
-    cs.fetch(if_label) { |k| cs[k] = Concept.new(if_label, 'interface:Interface')}
+    if_c = cs.fetch(if_label) { |k| cs[k] = Concept.new(if_label, 'interface:Interface')}
+    if_c.types << provides_if_rest_c.name
   end
 
   if (io_cell = row['Configured Input Output Name'])
@@ -81,7 +92,8 @@ table.each do |row|
 
   if (port_cell = row['Configured Port Name'])
     port_label = port_cell.to_s.split('::').last.gsub(/ \[.*\z/, '')
-    cs.fetch(port_label) { |k| cs[k] = Concept.new(port_label, 'interface:Port')}
+    port_c = cs.fetch(port_label) { |k| cs[k] = Concept.new(port_label, 'interface:Port')}
+    port_c.types << int_thr_port_rest_c.name
   end
 
   if (soa_cell = row['Configured SOA Name'])
